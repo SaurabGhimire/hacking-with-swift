dc6fe2047f51407f029d62119164984dbe543e6a
Saurab Ghimire <sourrab@gmail.com>
2024-05-31 19:04:22 -0500

    closures

--- checkpoint5/closures.playground/Contents.swift
+++ checkpoint5/closures.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+luckyNumbers
+.filter{
+    return !$0.isMultiple(of: 2)
+}
+.sorted()
+.map{
+        "\($0) is a lucky number"
+}
+.forEach{
+    print($0)
+}
+
+
+
+
