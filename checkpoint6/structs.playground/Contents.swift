1a242e621b09c4c442fb30522a6da3f21ff62627
Saurab Ghimire <sourrab@gmail.com>
2024-06-03 18:43:33 -0500

    struct

--- checkpoint6/structs.playground/Contents.swift
+++ checkpoint6/structs.playground/Contents.swift
@@ -1,3 +1,51 @@
 import UIKit
 
-var greeting = "Hello, playground"
+enum GearChange{
+    case up;
+    case down;
+}
+
+enum GearChangeError : Error{
+    case outOfRange
+}
+
+struct Car {
+    var model:String;
+    var noOfSeats:Int;
+    var currentGear = 1{
+        didSet{
+            print(currentGear)
+        }
+    };
+    
+    mutating func changeGear(input: GearChange) throws {
+        print("inside change gear")
+        
+        if(input == GearChange.up){
+            if(currentGear == 10){
+                throw GearChangeError.outOfRange;
+                
+            }
+            currentGear+=1;
+            return;
+        }
+        if(currentGear == 1){
+            throw GearChangeError.outOfRange;
+        }
+        currentGear-=1;
+    }
+}
+
+var car = Car(model: "Tesla", noOfSeats:4);
+
+
+for i in 1...13{
+    do{
+        try car.changeGear(input: GearChange.up)
+    }
+    catch  GearChangeError.outOfRange{
+        print("Gear is out of range");
+    }catch{
+        print("Something went wrong");
+    }
+}
