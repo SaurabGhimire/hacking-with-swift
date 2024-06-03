6e4bee4b9ea0f48fcbb8e31f43bd99c8bc09ec7c
Saurab Ghimire <sourrab@gmail.com>
2024-06-03 18:43:50 -0500

    classes

--- checkpoint7/classes.playground/Contents.swift
+++ checkpoint7/classes.playground/Contents.swift
@@ -1,3 +1,60 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class Animal {
+    let legs: Int;
+    init(legs: Int){
+        self.legs = legs;
+    }
+    
+    func speak(){
+        print("Animals can't speak")
+    }
+    
+    
+}
+
+class Dog: Animal{
+    override init(legs: Int) {
+        super.init(legs: legs);
+        
+    }
+    override func speak(){
+        print("Dogs bark!!")
+    }
+}
+
+class Cat: Animal{
+    let isTame: Bool
+    init(legs: Int, isTame:  Bool){
+        self.isTame = isTame;
+        super.init(legs: legs)
+    }
+    
+    override func speak(){
+        print("Cats meow!!")
+    }
+}
+
+class Corgi: Dog{
+    override init(legs: Int){
+        super.init(legs: legs)
+    }
+}
+
+class Poodle: Dog{
+    override init(legs: Int){
+        super.init(legs: legs)
+    }
+}
+
+class Persian: Cat{
+     override init(legs: Int, isTame: Bool) {
+         super.init(legs: legs, isTame: isTame)
+    }
+}
+
+class Lion: Cat{
+     override init(legs: Int, isTame: Bool) {
+         super.init(legs: legs, isTame: isTame)
+    }
+}
